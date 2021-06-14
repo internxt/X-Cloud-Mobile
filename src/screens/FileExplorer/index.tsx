a592f086afc349053c1220106623c5644c891bc1
Desislava <demidietsinf@gmail.com>
2021-06-14 14:28:27 +0200

    refactor fileExplorer

--- src/screens/FileExplorer/index.tsx
+++ src/screens/FileExplorer/index.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
-import { Text, View, StyleSheet, Image, Platform, Alert, BackHandler } from 'react-native'
+import { Text, View, StyleSheet, Image, Platform, BackHandler } from 'react-native'
 import AppMenu from '../../components/AppMenu'
-import { fileActions, userActions } from '../../redux/actions';
+import { fileActions } from '../../redux/actions';
 import { connect } from 'react-redux';
 import FileList from '../../components/FileList';
 import SettingsModal from '../../modals/SettingsModal';
@@ -13,19 +13,15 @@ import DeleteItemModal from '../../modals/DeleteItemModal';
 import MoveFilesModal from '../../modals/MoveFilesModal';
 import ShareFilesModal from '../../modals/ShareFilesModal';
 import { Reducers } from '../../redux/reducers/reducers';
-import analytics, { getLyticsData } from '../../helpers/lytics';
-import RNFetchBlob from 'rn-fetch-blob';
 import { WaveIndicator } from 'react-native-indicators'
-import Toast from 'react-native-simple-toast'
 import FreeForYouModal from '../../modals/FreeForYouModal';
 import strings from '../../../assets/lang/strings';
 import { StackNavigationProp } from 'react-navigation-stack/lib/typescript/src/vendor/types';
 import * as fileExplorerService from '../../services/fileExplorer';
-
 interface FileExplorerProps extends Reducers {
   navigation?: StackNavigationProp
   filesState: any
-  dispatch?: any,
+  dispatch?: any
   layoutState: any
   authenticationState: any
 }
@@ -40,17 +36,7 @@ function FileExplorer(props: FileExplorerProps): JSX.Element {
       return null
     }
   })()
-  let count = 0
-
-  // Check if everything is set up for file upload
-  const validateUri = () => {
-    if (Platform.OS === 'ios') {
-      return filesState.uri && filesState.folderContent && filesState.folderContent.currentFolder
-
-    } else {
-      return filesState.uri.fileUri && filesState.folderContent && filesState.folderContent.currentFolder
-    }
-  }
+  const count = 0
 
   useEffect(() => {
     fileExplorerService.refreshLimitStorage().then(limitValue =>
@@ -59,145 +45,20 @@ function FileExplorer(props: FileExplorerProps): JSX.Element {
 
   // useEffect to trigger uploadFile while app on background
   useEffect(() => {
-    if (Platform.OS === 'ios') {
-      if (filesState.uri && validateUri()) {
-        const uri = filesState.uri
-        const name = filesState.uri.split('/').pop()
-
-        setTimeout(() => {
-          uploadFile(uri, name, filesState.folderContent.currentFolder)
-        }, 3000)
-      }
-    } else {
-      if (filesState.uri && validateUri()) {
-        const uri = filesState.uri.fileUri
-        const name = filesState.uri.fileName.split('/').pop()
-
-        setTimeout(() => {
-          uploadFile(uri, name, filesState.folderContent.currentFolder)
-        }, 3000)
-      }
-    }
+    fileExplorerService.uploadFileOnBackground(filesState, props.dispatch);
   }, [filesState.uri])
 
   // seEffect to trigger uploadFile while app closed
   useEffect(() => {
-    if (Platform.OS === 'ios') {
-      if (validateUri()) {
-        const uri = filesState.uri
-        const name = filesState.uri.split('/').pop()
-
-        setTimeout(() => {
-          uploadFile(uri, name, filesState.folderContent.currentFolder)
-        }, 3000)
-      }
-    } else {
-      if (filesState.uri && validateUri()) {
-        const uri = filesState.uri.fileUri
-        const name = filesState.uri.fileName
-
-        setTimeout(() => {
-          uploadFile(uri, name, filesState.folderContent.currentFolder)
-        }, 3000)
-      }
-    }
+    fileExplorerService.uploadFileWhileAppClosed(filesState, props.dispatch);
 
     // Set rootfoldercontent for MoveFilesModal
-    parentFolderId === null ? props.dispatch(fileActions.setRootFolderContent(filesState.folderContent)) : null
-
-    // BackHandler
-    const backAction = () => {
-      if (props.filesState.folderContent && !props.filesState.folderContent.parentId) {
-        count++
-        if (count < 2) {
-          Toast.show('Try exiting again to close the app')
-        } else {
-          BackHandler.exitApp()
-        }
-
-        // Reset if some time passes
-        setTimeout(() => {
-          count = 0
-        }, 4000)
-      } else {
-        props.dispatch(fileActions.getFolderContent(props.filesState.folderContent.parentId))
-      }
-      return true
-    }
-    const backHandler = BackHandler.addEventListener('hardwareBackPress', backAction)
+    fileExplorerService.setParentFolderId(filesState, parentFolderId, props.dispatch);
+    const actionBack = fileExplorerService.backAction(filesState, count, props.dispatch);
+    const backHandler = BackHandler.addEventListener('hardwareBackPress', actionBack);
 
     return () => backHandler.remove()
   }, [filesState.folderContent])
-
-  const uploadFile = async (uri: string, name: string, currentFolder: number) => {
-    props.dispatch(fileActions.setUri(undefined))
-    const userData = await getLyticsData()
-
-    try {
-      const token = props.authenticationState.token;
-      const mnemonic = props.authenticationState.user.mnemonic;
-      const headers = {
-        'Authorization': `Bearer ${token}`,
-        'internxt-mnemonic': mnemonic,
-        'Content-Type': 'multipart/form-data'
-      }
-      const regex = /^(.*:\/{0,2})\/?(.*)$/gm
-
-      analytics.track('file-upload-start', { userId: userData.uuid, email: userData.email, device: 'mobile' }).catch(() => { })
-      props.dispatch(fileActions.uploadFileStart(name))
-
-      const file = uri.replace(regex, '$2') // if iOS remove file://
-      const finalUri = Platform.OS === 'ios' ? RNFetchBlob.wrap(decodeURIComponent(file)) : RNFetchBlob.wrap(uri)
-
-      RNFetchBlob.fetch('POST', `${process.env.REACT_NATIVE_API_URL}/api/storage/folder/${currentFolder}/upload`, headers,
-        [
-          { name: 'xfile', filename: name, data: finalUri }
-        ])
-        .uploadProgress({ count: 10 }, (sent, total) => {
-          props.dispatch(fileActions.uploadFileSetProgress(sent / total))
-
-        })
-        .then((res) => {
-          if (res.respInfo.status === 401) {
-            throw res;
-          }
-          const data = res
-
-          return data
-        })
-        .then(res => {
-          if (res.respInfo.status === 402) {
-            props.navigation.replace('OutOfSpace')
-
-          } else if (res.respInfo.status === 201) {
-            analytics.track('file-upload-finished', { userId: userData.uuid, email: userData.email, device: 'mobile' }).catch(() => { })
-            props.dispatch(fileActions.getFolderContent(filesState.folderContent.currentFolder))
-
-          } else {
-            Alert.alert('Error', 'Can not upload file');
-          }
-
-          props.dispatch(fileActions.uploadFileSetProgress(0))
-          props.dispatch(fileActions.uploadFileFinished());
-        })
-        .catch((err) => {
-          if (err.status === 401) {
-            props.dispatch(userActions.signout())
-
-          } else {
-            Alert.alert('Error', 'Cannot upload file\n' + err.message)
-          }
-
-          props.dispatch(fileActions.uploadFileFailed())
-          props.dispatch(fileActions.uploadFileFinished())
-        })
-
-    } catch (error) {
-      analytics.track('file-upload-error', { userId: userData.uuid, email: userData.email, device: 'mobile' }).catch(() => { })
-      props.dispatch(fileActions.uploadFileFailed())
-      props.dispatch(fileActions.uploadFileFinished())
-    }
-  }
 
   useEffect(() => {
     const keyId = filesState.selectedItems.length > 0 && filesState.selectedItems[0].id
@@ -205,7 +66,7 @@ function FileExplorer(props: FileExplorerProps): JSX.Element {
     setSelectedKeyId(keyId)
   }, [filesState])
 
-  if (!props.authenticationState.loggedIn) {
+  if (!props.authenticationState.loggedIn && props.navigation) {
     props.navigation.replace('Login')
   }
 
