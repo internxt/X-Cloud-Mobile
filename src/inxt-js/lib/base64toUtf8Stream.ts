928db06cfe825fc7f87930e57eef2befc2fa4fa9
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2021-07-06 15:23:01 +0200

    base64 to utf8 stream working

--- src/inxt-js/lib/base64toUtf8Stream.ts
+++ src/inxt-js/lib/base64toUtf8Stream.ts
@@ -1,26 +1,13 @@
-import { Transform } from 'readable-stream';
+import { Transform, Duplex } from 'readable-stream';
+
+type BufferEncoding = 'ascii' | 'utf8' | 'utf-8' | 'utf16le' | 'ucs2' | 'ucs-2' | 'base64' | 'latin1' | 'binary' | 'hex';
 
 export class Base64ToUtf8Transform extends Transform {
-  private lastChunk: Buffer;
-  private highWatermark: number;
-
-  constructor(highWatermark: number) {
-    super();
-    this.highWatermark = highWatermark;
-  }
-
-  _transform(chunk: Buffer, enc: string, cb: (err: Error | null, data: Buffer) => void): void {
-    // chunk is encoded in base64
-    const lastChunkToUtf8 = Buffer.from(chunk.toString('utf8'), 'utf8');
-
-    this.lastChunk = lastChunkToUtf8.slice(this.highWatermark);
-    cb(null, Buffer.from(lastChunkToUtf8.slice(0, this.highWatermark)));
-  }
-
-  _flush(cb: (err: Error | null, data: Buffer) => void): void {
-    if (this.lastChunk.length > 0) {
-      this.push(this.lastChunk, 'utf8');
+  push(chunk: Buffer, encoding?: BufferEncoding): boolean {
+    if (chunk) {
+      return Duplex.prototype.push.call(this, chunk, encoding);
     }
-    cb(null, null);
+
+    return Duplex.prototype.push.call(this, null, encoding);
   }
 }
